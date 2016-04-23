87a221b78a7ee2a006b9fa1b421e206e897030b2
Chris Spanos <spanosc1@tcnj.edu>
2016-04-23 00:36:44 -0400

    part 1 complete

---
+++ BamazonCustomer.js
@@ -0,0 +1,56 @@
+var mysql = require('mysql');
+var prompt = require('prompt');
+var ID;
+var quantity;
+var con = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '13748abc',
+	database : 'bamazon'
+});
+
+con.connect(function (err)
+{
+	if (err)
+	{
+		console.log(err);
+	}
+	console.log("Welcome to Bamazon!");
+});
+
+con.query('SELECT * FROM products', function(err, rows){
+	if (err)
+	{
+    return callback(err);
+  }
+  console.log("Item ID\tItem Name\t\tPrice");
+  for(var i = 0; i < rows.length; i++)
+  {
+  	console.log(rows[i].ItemID + '\t' + rows[i].ProductName + '\t\t$' + rows[i].Price + '\t');
+  }
+  console.log("\nWhat would you like to order?\nEnter the item ID number and a quantity.");
+	prompt.start();
+	prompt.get(['ItemID', 'quantity'], function(err, order){
+		ID = order.ItemID;
+		quantity = order.quantity;
+		prompt.stop();
+		con.query('SELECT * FROM products WHERE ItemID = "' + ID + '"', function(err, product){
+  		if(product[0].StockQuantity >= quantity)
+  		{
+  			var newQuant = product[0].StockQuantity - quantity;
+  			con.query('UPDATE products SET StockQuantity = "' + newQuant + '" WHERE ItemID = "' + ID + '"', function(err, result){
+  				if(err)
+  				{
+  					throw err;
+  				}
+  			});
+  			console.log("You ordered " + quantity + " of this item: " + product[0].ProductName);
+				console.log("Your total is: $" + product[0].Price * quantity);
+			}
+			else
+			{
+				console.log("Insufficient Quantity.  Transaction could not be completed.");
+			}
+		});
+	});
+});
